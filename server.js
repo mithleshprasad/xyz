d39179a697db18b95ac6b38d6dcf3471e78b3202
mithlesh prasad <101449591+mithleshprasad@users.noreply.github.com>
2025-05-11 22:34:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://xyz-0576.onrender.com',
   credentials: true
 }));
 
